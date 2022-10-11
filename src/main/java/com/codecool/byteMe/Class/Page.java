60e9c88470490c2c446fca60742b988c13e0dfcb
paethrick <izelierik9@gmail.com>
2022-10-11 17:17:01 +0200

    feat: basic user routs added
    -get: getUser
    -post: addUser
    -put: updateName

--- src/main/java/com/codecool/byteMe/Class/Page.java
+++ src/main/java/com/codecool/byteMe/Class/Page.java
@@ -1,8 +1,11 @@
 package com.codecool.byteMe.Class;
 
+import org.springframework.stereotype.Component;
+
 import java.util.ArrayList;
 import java.util.List;
 
+@Component
 public class Page {
 
     private List<User> users;
@@ -13,6 +16,18 @@ public class Page {
 
     public void addUser(User user) {
         users.add(user);
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void updateName(User user, String name){
+        for(User updateUser : users){
+            if(updateUser.getName().equals(user.getName())){
+                updateUser.setName(name);
+            }
+        }
     }
 
     public List<Post> displayPosts() {
