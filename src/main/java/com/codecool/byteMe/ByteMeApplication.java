fe295c7540ced65a600a64462d14cce1a54bc510
feketedaniel <danielt.fekete@gmail.com>
2022-10-11 11:47:55 +0200

    build(app): initialize spring project
    -closes #1
    -closes #2
    -closes #3
    -closes #4

---
+++ src/main/java/com/codecool/byteMe/ByteMeApplication.java
@@ -0,0 +1,13 @@
+package com.codecool.byteMe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ByteMeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ByteMeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codecool/byteMe/ByteMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codecool.byteMe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ByteMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
