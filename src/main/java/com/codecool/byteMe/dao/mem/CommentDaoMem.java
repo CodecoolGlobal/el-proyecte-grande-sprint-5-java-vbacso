90ff8df7a2ec3d5cbeca1b6c1655075293da8d85
fergencszeno <fergencszeno@gmail.com>
2022-10-12 11:56:45 +0200

    feat: create methods for dao mem classes

--- src/main/java/com/codecool/byteMe/dao/mem/CommentDaoMem.java
+++ src/main/java/com/codecool/byteMe/dao/mem/CommentDaoMem.java
@@ -1,6 +1,33 @@
 package com.codecool.byteMe.dao.mem;
 
 import com.codecool.byteMe.dao.CommentDao;
+import com.codecool.byteMe.model.postable.Comment;
+
+import java.util.Set;
+import java.util.UUID;
 
 public class CommentDaoMem implements CommentDao {
+    @Override
+    public Comment add(Comment comment) {
+        throw new UnsupportedOperationException("Not implemented method: (add) in class: (CommentDaoMem)");
+        //TODO: (fergencszeno, 2022. 10. 12.) Implement method: (add) for class: (CommentDaoMem)
+    }
+
+    @Override
+    public Set<Comment> findByPost(UUID postId) {
+        throw new UnsupportedOperationException("Not implemented method: (findByPost) in class: (CommentDaoMem)");
+        //TODO: (fergencszeno, 2022. 10. 12.) Implement method: (findByPost) for class: (CommentDaoMem)
+    }
+
+    @Override
+    public Comment editComment(Comment comment) {
+        throw new UnsupportedOperationException("Not implemented method: (editComment) in class: (CommentDaoMem)");
+        //TODO: (fergencszeno, 2022. 10. 12.) Implement method: (editComment) for class: (CommentDaoMem)
+    }
+
+    @Override
+    public void deleteComment(UUID commentId) {
+        throw new UnsupportedOperationException("Not implemented method: (deleteComment) in class: (CommentDaoMem)");
+        //TODO: (fergencszeno, 2022. 10. 12.) Implement method: (deleteComment) for class: (CommentDaoMem)
+    }
 }
--- src/main/java/com/codecool/byteMe/dao/mem/UserDaoMem.java
+++ src/main/java/com/codecool/byteMe/dao/mem/UserDaoMem.java
@@ -1,6 +1,18 @@
 package com.codecool.byteMe.dao.mem;
 
 import com.codecool.byteMe.dao.UserDao;
+import com.codecool.byteMe.model.User;
 
 public class UserDaoMem implements UserDao {
+    @Override
+    public User add(User user) {
+        throw new UnsupportedOperationException("Not implemented method: (add) in class: (UserDaoMem)");
+        //TODO: (fergencszeno, 2022. 10. 12.) Implement method: (add) for class: (UserDaoMem)
+    }
+
+    @Override
+    public User find(String email) {
+        throw new UnsupportedOperationException("Not implemented method: (find) in class: (UserDaoMem)");
+        //TODO: (fergencszeno, 2022. 10. 12.) Implement method: (find) for class: (UserDaoMem)
+    }
 }
