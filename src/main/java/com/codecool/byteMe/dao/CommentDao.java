f95dfe2b832ee0ddc54fe5ec24cf90e391b41643
fergencszeno <fergencszeno@gmail.com>
2022-10-12 11:49:50 +0200

    feat: create methods for dao interfaces

--- src/main/java/com/codecool/byteMe/dao/CommentDao.java
+++ src/main/java/com/codecool/byteMe/dao/CommentDao.java
@@ -1,4 +1,16 @@
 package com.codecool.byteMe.dao;
 
+import com.codecool.byteMe.model.postable.Comment;
+
+import java.util.Set;
+import java.util.UUID;
+
 public interface CommentDao {
+    Comment add(Comment comment);
+
+    Set<Comment> findByPost(UUID postId);
+
+    Comment editComment(Comment comment);
+
+    void deleteComment(UUID commentId);
 }
--- src/main/java/com/codecool/byteMe/dao/UserDao.java
+++ src/main/java/com/codecool/byteMe/dao/UserDao.java
@@ -1,4 +1,9 @@
 package com.codecool.byteMe.dao;
 
+import com.codecool.byteMe.model.User;
+
 public interface UserDao {
+    User add(User user);
+
+    User find(String email);
 }
