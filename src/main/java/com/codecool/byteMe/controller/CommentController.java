09335e86802405f060671243fe6203307bab28df
feketedaniel <danielt.fekete@gmail.com>
2022-10-13 14:17:02 +0200

    feat: add routes to CommentController

--- src/main/java/com/codecool/byteMe/controller/CommentController.java
+++ src/main/java/com/codecool/byteMe/controller/CommentController.java
@@ -1,4 +1,44 @@
 package com.codecool.byteMe.controller;
 
+import com.codecool.byteMe.dao.AppDao;
+import com.codecool.byteMe.model.postable.Comment;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Set;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/comment")
 public class CommentController {
+
+    private AppDao appDao;
+
+    private CommentController(AppDao appDao) {
+        this.appDao = appDao;
+    }
+
+    @GetMapping("getAllComment")
+    public Set<Comment> getAllComment() {
+        return appDao.getAllComment();
+    }
+
+    @GetMapping("find/{commentId}")
+    public Comment find(@PathVariable UUID commentId) {
+        return appDao.findCommentById(commentId);
+    }
+
+    @PostMapping("add")
+    public Comment add(@RequestBody Comment comment) {
+        return appDao.addComment(comment);
+    }
+
+    @PutMapping("update")
+    public Comment update(@RequestBody Comment comment) {
+        return appDao.editComment(comment);
+    }
+
+    @DeleteMapping("delete")
+    public Comment delete(@RequestBody Comment comment) {
+        return appDao.deleteComment(comment.getId());
+    }
 }
