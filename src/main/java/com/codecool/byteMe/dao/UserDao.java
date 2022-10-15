eba493cd2dc97d2ac1ab1a5f8781632020e2ac90
feketedaniel <danielt.fekete@gmail.com>
2022-10-15 12:27:07 +0200

    refactor: remove unused methods from UserDaoMem and UserDao

--- src/main/java/com/codecool/byteMe/dao/UserDao.java
+++ src/main/java/com/codecool/byteMe/dao/UserDao.java
@@ -1,15 +1,12 @@
 package com.codecool.byteMe.dao;
 
 import com.codecool.byteMe.model.User;
-import com.codecool.byteMe.model.postable.Comment;
 
 import java.util.Set;
 import java.util.UUID;
 
 public interface UserDao {
     Set<User> getAll();
-
-    Set<Comment> getAllComment();
 
     User add(User user);
 
@@ -20,18 +17,4 @@ public interface UserDao {
     User findById(UUID userId);
 
     User delete(UUID userId);
-
-
-    Comment addComment(Comment comment);
-
-    Set<Comment> findCommentsByUser(User user);
-
-    Set<Comment> findCommentsByPostId(UUID postId);
-
-    Comment findCommentById(UUID commentId);
-
-    Comment editComment(Comment comment);
-
-    Comment deleteComment(UUID commentId);
-
 }
--- src/main/java/com/codecool/byteMe/dao/mem/UserDaoMem.java
+++ src/main/java/com/codecool/byteMe/dao/mem/UserDaoMem.java
@@ -2,7 +2,6 @@ package com.codecool.byteMe.dao.mem;
 
 import com.codecool.byteMe.dao.UserDao;
 import com.codecool.byteMe.model.User;
-import com.codecool.byteMe.model.postable.Comment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -11,7 +10,6 @@ import java.util.HashSet;
 import java.util.NoSuchElementException;
 import java.util.Set;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 
 @Component
@@ -42,7 +40,10 @@ public class UserDaoMem implements UserDao {
 
     @Override
     public User edit(User user) {
-        data.stream().filter(userU -> userU.getId().equals(user.getId())).findFirst().orElseThrow(NoSuchElementException::new)
+        data.stream()
+                .filter(userU -> userU.getId().equals(user.getId()))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new)
                 .setName(user.getName());
         return user;
     }
@@ -65,81 +66,11 @@ public class UserDaoMem implements UserDao {
 
     @Override
     public User delete(UUID userId) {
-        User userToDelete = data.stream().filter(user -> user.getId().equals(userId)).findFirst().orElseThrow(NoSuchElementException::new);
+        User userToDelete = data.stream()
+                .filter(user -> user.getId().equals(userId))
+                .findFirst()
+                .orElseThrow(NoSuchElementException::new);
         data.remove(userToDelete);
         return userToDelete;
-    }
-
-
-    @Override
-    public Set<Comment> getAllComment() {
-        return data.stream()
-                .map(User::getComments)
-                .flatMap(Set::stream)
-                .collect(Collectors.toSet());
-    }
-
-    @Override
-    public Comment addComment(Comment comment) {
-        if (comment.getPostId() == null && comment.getUserId() == null)
-            throw new IllegalArgumentException("Missing object attribute: userId and postId can't be null.");
-        data.stream()
-                .filter(user -> user.getId().equals(comment.getUserId()))
-                .findFirst().orElseThrow(NoSuchElementException::new)
-                .addComment(comment);
-        data.stream()
-                .map(User::getPosts)
-                .flatMap(Set::stream)
-                .filter(post -> post.getId().equals(comment.getPostId()))
-                .findFirst().orElseThrow(NoSuchElementException::new)
-                .addComment(comment);
-        return comment;
-    }
-
-    @Override
-    public Comment findCommentById(UUID commentId) {
-        return data.stream()
-                .map(User::getComments)
-                .flatMap(Set::stream)
-                .filter(comment -> comment.getId().equals(commentId))
-                .findFirst().orElseThrow(NoSuchElementException::new);
-    }
-
-    @Override
-    public Set<Comment> findCommentsByUser(User user) {
-        return data.stream()
-                .filter(user1 -> user1.getId().equals(user.getId()))
-                .findFirst().orElseThrow(NoSuchElementException::new)
-                .getComments();
-    }
-
-    @Override
-    public Set<Comment> findCommentsByPostId(UUID postId) {
-        return getAllComment().stream()
-                .filter(comment -> comment.getPostId().equals(postId))
-                .collect(Collectors.toSet());
-    }
-
-    @Override
-    public Comment editComment(Comment comment) {
-        Comment editableComment = findCommentById(comment.getId());
-        editableComment.editComment(comment);
-        return editableComment;
-    }
-
-    @Override
-    public Comment deleteComment(UUID commentId) {
-        Comment comment = findCommentById(commentId);
-        data.stream()
-                .filter(user -> user.getId().equals(comment.getUserId()))
-                .findFirst().orElseThrow(NoSuchElementException::new)
-                .removeComment(comment);
-        data.stream()
-                .map(User::getPosts)
-                .flatMap(Set::stream)
-                .filter(post -> post.getId().equals(comment.getPostId()))
-                .findFirst().orElseThrow(NoSuchElementException::new)
-                .removeComment(comment);
-        return comment;
     }
 }
