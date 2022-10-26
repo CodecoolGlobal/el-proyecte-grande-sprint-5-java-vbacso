0ab0a3fd5a7c64f994d0689206fb49bce29a8b4b
fergencszeno <fergencszeno@gmail.com>
2022-10-26 13:12:15 +0200

    feat: show feed page content

--- byteme/src/components/feed_page/FeedPage.js
+++ byteme/src/components/feed_page/FeedPage.js
@@ -1,10 +1,27 @@
-import NavigationBar from "../navbar/NavigationBar";
 import Feed from "./Feed";
+import {useEffect, useState} from "react";
 
-const FeedPage = () => {
+const FeedPage = ({loggedInUserId}) => {
+    const [posts, setPosts] = useState([]);
+    useEffect(() => {
+        const getPosts = async () => {
+            const resp = await fetch(`http://localhost:8080/post/friends/${loggedInUserId}`);
+            if (resp.ok) {
+                const fetchedPosts = await resp.json();
+                setPosts(fetchedPosts);
+            }
+        }
+        getPosts().catch(console.error);
+    }, []);
+
     return (
         <div>
-            FeedPageLoaded
+            {posts.map((post) => (
+                <div className="post" key={post.id}>
+                    <p>{post.userId}</p>
+                    <p>{post.title}</p>
+                </div>
+            ))}
             <Feed/>
         </div>
     );
