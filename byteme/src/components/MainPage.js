286c4668fb8f59b111b1567b7dd366c23508e85a
fergencszeno <fergencszeno@gmail.com>
2022-11-08 11:19:36 +0100

    feat: insert EditProfilePage component

--- byteme/src/components/MainPage.js
+++ byteme/src/components/MainPage.js
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import NavigationBar from "./navbar/NavigationBar";
 import FeedPage from "./feed_page/FeedPage";
 import UserPage from "./user_page/UserPage";
+import EditProfilePage from "./user_page/EditProfilePage";
 
 const MainPage = ({loggedInUserId, onLogout}) => {
     const [showContent, setShowContent] = useState("feedPage")
@@ -14,12 +15,18 @@ const MainPage = ({loggedInUserId, onLogout}) => {
         e.preventDefault();
         setShowContent("userPage")
     };
+    const loadEditProfile = (e) => {
+        e.preventDefault();
+        setShowContent("editProfilePage")
+    }
 
     const routeController = () => {
         if (showContent === "feedPage") {
             return <FeedPage loggedInUserId={loggedInUserId}/>
         } else if (showContent === "userPage") {
-            return <UserPage/>
+            return <UserPage loadEditProfile={loadEditProfile}/>
+        } else if (showContent === "editProfilePage") {
+            return <EditProfilePage/>
         }
     };
 
