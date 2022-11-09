911c1d7bec4cfe84fe6c84fdb52aad500cbf0273
feketedaniel <danielt.fekete@gmail.com>
2022-11-09 12:21:33 +0100

    feat: results append if present

--- byteme/src/components/navbar/searchbar/SearchBar.js
+++ byteme/src/components/navbar/searchbar/SearchBar.js
@@ -5,24 +5,39 @@ const SearchBar = () => {
 
     const [searchResultUsers, setSearchResultUsers] = useState();
     useEffect(() => {
-
     });
     const fetchSearchResult = async (param) => {
-        console.log("search: " + param)
+        console.log("search: " + param);
+        setSearchResultUsers([
+            {"id": 1, "name": "result1"},
+            {"id": 2, "name": "result2"}
+        ]);
     };
 
     const onChangeEvent = (e) => {
         e.preventDefault();
         const text = e.target.value.trim();
         if (text) {
-            fetchSearchResult(text).catch(console.error)
+            fetchSearchResult(text).catch(console.error);
+        } else {
+            setSearchResultUsers();
         }
     };
 
     return (
-        <div className="input-group input-group-sm searchbar">
-            <input type="text" className="form-control" placeholder="Write here to search" onChange={onChangeEvent}/>
-            <button className="btn btn-sm button-search"><BiSearchAlt2/></button>
+        <div className="searchbar-position-fixer">
+        <div className="searchbar-container">
+            <div className="input-group input-group-sm searchbar-input">
+                <input type="text" className="form-control" placeholder="Search on byte.me" onChange={onChangeEvent}/>
+                <button className="btn btn-sm button-search"><BiSearchAlt2/></button>
+            </div>
+                <div className="searchbar-results-container">
+                    <ul className="searchbar-results-list">
+                        {searchResultUsers?.map((entry) => (
+                            <li key={entry.id} className="searchbar-results-item">{entry.name}</li>))}
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
