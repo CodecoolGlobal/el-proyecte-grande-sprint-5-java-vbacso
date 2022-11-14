186d2f802179922f3ee0dc1c3a5e2b6a2e4f4c4a
fergencszeno <fergencszeno@gmail.com>
2022-11-14 11:48:37 +0100

    feat: create Loading component

---
+++ byteme/src/components/common/Loading.js
@@ -0,0 +1,10 @@
+const Loading = () => {
+    return (
+        <div>
+            <i className="fa fa-refresh fa-spin fa-3x fa-fw"></i>
+            <span className="sr-only">Loading...</span>
+        </div>
+    );
+};
+
+export default Loading;
