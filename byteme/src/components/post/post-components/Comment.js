8fd9d7a47ae1a573b3560ab9ee53693b3d8eab71
vbacso <bacsovanda95@gmail.com>
2022-11-15 13:07:07 +0100

    feat: add profile picture to comments and posts

--- byteme/src/components/post/post-components/Comment.js
+++ byteme/src/components/post/post-components/Comment.js
@@ -1,6 +1,9 @@
-const Comment = ({  name,body, last }) => {
+import ProfilePicture from "../../user_page/ProfilePicture";
+
+const Comment = ({name, body, last, profilePictureID}) => {
     return (
-        <div className={last ? 'comments-container' : 'comments-container last'} style={{color:'white'}}>
+        <div className={last ? 'comments-container' : 'comments-container last'} style={{color: 'white'}}>
+            <ProfilePicture profilePictureId={profilePictureID} placement={"post"}/>
             <p>{name}</p>
             <p>{body}</p>
         </div>
--- byteme/src/components/post/post-components/PostHeader.js
+++ byteme/src/components/post/post-components/PostHeader.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {FaTimes} from "react-icons/fa";
+import ProfilePicture from "../../user_page/ProfilePicture";
 
-const PostHeader = ({postId, userName, created, title, userId, onDelete}) => {
+const PostHeader = ({postId, userName, created, title, userId, onDelete, profilePictureId}) => {
     const loggedInUserId = JSON.parse(localStorage.getItem("loggedInUser")).id;
     return (
         <div className='post-header'>
+            <ProfilePicture placement={"post"} profilePictureId={profilePictureId}/>
             <h6 className='username-in-post'>
                 {userName + ' - '
                     + new Date(created).toLocaleTimeString('en', {
@@ -17,7 +19,6 @@ const PostHeader = ({postId, userName, created, title, userId, onDelete}) => {
             <h4 className='post-title'>{title}</h4>
             {loggedInUserId === userId ?
                 <FaTimes onClick={() => onDelete(postId)} className='delete-icon' cursor='pointer'/> : ''}
-
         </div>
     );
 };
