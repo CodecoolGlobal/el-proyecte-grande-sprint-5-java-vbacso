be565e228d1144fa93652608eb528e0e6ccf2b93
fergencszeno <fergencszeno@gmail.com>
2022-11-16 12:31:47 +0100

    feat: create friend component

---
+++ byteme/src/components/user_page/Friend.js
@@ -0,0 +1,34 @@
+import AddFriendButton from "./AddFriendButton";
+import DeleteFriendButton from "./DeleteFriendButton";
+
+const Friend = ({loggedInUser, setLoggedInUser, showedUser, setShowedUser}) => {
+    const loggedInUserId = loggedInUser.id;
+    const showedUserId = showedUser.id;
+
+    const isFriend = () => {
+        let isFriend = false;
+        for (let loggedInUserFriend of loggedInUser.friendList) {
+            if (loggedInUserFriend.id === showedUserId) {
+                isFriend = true;
+            }
+        }
+        for (let showedUserFriend of showedUser.friendList) {
+            if (showedUserFriend.id === loggedInUserId) {
+                isFriend = true;
+            }
+        }
+        return isFriend;
+    };
+
+    return (
+        <div>
+            {loggedInUserId === showedUserId ? ""
+                : (loggedInUserId !== showedUserId && isFriend() === false) ?
+                    <AddFriendButton loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser}
+                                     showedUser={showedUser} setShowedUser={setShowedUser}/>
+                    : <DeleteFriendButton/>}
+        </div>
+    );
+};
+
+export default Friend;
