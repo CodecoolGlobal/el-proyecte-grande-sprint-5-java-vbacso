b61257d06a26edd4a349d00ea30a0cec6298a4ff
feketedaniel <danielt.fekete@gmail.com>
2022-11-22 16:06:03 +0100

    fix: compile error

--- byteme/src/index.js
+++ byteme/src/index.js
@@ -5,7 +5,6 @@ import App from './App';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.min.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
