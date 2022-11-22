397b93d745dc6a4c9d0de17d972dbc779e72aa22
fergencszeno <fergencszeno@gmail.com>
2022-11-22 13:41:23 +0100

    feat: implement onLoadUserPage function

--- byteme/src/components/user_page/ProfilePicture.js
+++ byteme/src/components/user_page/ProfilePicture.js
@@ -1,8 +1,7 @@
 import {useEffect, useState} from "react";
 
-const ProfilePicture = ({profilePictureId, placement}) => {
+const ProfilePicture = ({userId, profilePictureId, placement, setShowedUser}) => {
     const [image, setImage] = useState();
-
     useEffect(() => {
         const fetchImage = async () => {
             const resp = await fetch(`http://localhost:8080/image/${profilePictureId}`)
@@ -13,11 +12,21 @@ const ProfilePicture = ({profilePictureId, placement}) => {
         fetchImage().catch(console.error);
     }, [profilePictureId]);
 
+    const onLoadUserPage = async (e) => {
+        e.preventDefault();
+        const showedUserId = e.target.dataset.userId;
+        if (showedUserId !== undefined) {
+            const res = await fetch(`http://localhost:8080/user/findById/${showedUserId}`);
+            setShowedUser(await res.json());
+        }
+    }
+
     if (!image) {
         return (<div className="main-container">Loading...</div>)
     } else {
-        return (<div className={(placement === "post") ? "profile-pic-in-post" : "profile-pic-user-page"}>
-            <img className="rounded-circle ratio" src={image} alt="Profile Picture"/>
+        return (<div onClick={onLoadUserPage}
+                     className={(placement === "post") ? "profile-pic-in-post" : "profile-pic-user-page"}>
+            <img className="rounded-circle ratio" data-user-id={userId} src={image} alt="Profile Picture"/>
         </div>)
     }
 }
