4470b6e7fdf4d6acc08389d55fd4216602777869
vbacso <bacsovanda95@gmail.com>
2022-11-23 10:53:06 +0100

    fix: add custom ids to navigation link elements

--- byteme/src/components/navbar/LinkFeedPage.js
+++ byteme/src/components/navbar/LinkFeedPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const LinkFeedPage = ({loadFeedPage}) => {
-    return (<a className="nav-link" onClick={loadFeedPage}>byte.Feed</a>
+    return (<a className="nav-link" id="feed-page" onClick={loadFeedPage}>byte.Feed</a>
     );
 };
 
