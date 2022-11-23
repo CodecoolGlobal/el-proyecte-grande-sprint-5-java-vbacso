03ccc96ca80ccfab855e8d2887da6602e58ae8bb
vbacso <bacsovanda95@gmail.com>
2022-11-23 11:07:19 +0100

    feat: add password for login fetch and implement onRegistration fetch method

--- byteme/src/App.js
+++ byteme/src/App.js
@@ -3,7 +3,7 @@ import {useEffect, useState} from "react";
 import LoginPage from "./components/login/LoginPage";
 import {Navigate, Route, Routes, useNavigate} from "react-router-dom";
 import MainPage from "./components/MainPage";
-
+import RegistrationPage from "./components/registration/RegistrationPage";
 
 function App() {
 
@@ -18,13 +18,13 @@ function App() {
     }, [JSON.stringify(loggedInUser)])
 
 
-    const onLogin = async (email) => {
+    const onLogin = async (email, password) => {
         const resp = await fetch("http://localhost:8080/login", {
             method: "POST",
             headers: {
                 'Accept': 'application/json', 'Content-Type': 'application/json',
             },
-            body: JSON.stringify({"email": email})
+            body: JSON.stringify({"email": email, "password": password})
         })
         if (resp.ok) {
             const user = await resp.json();
@@ -34,18 +34,39 @@ function App() {
             alert("Invalid email!")
         }
     }
+
+    const onRegistration = async (email, password, name, age) => {
+        const resp = await fetch("http://localhost:8080/registration", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json', 'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({"email": email, "password": password, "name": name, "age": age})
+        })
+        if (resp.ok) {
+            const user = await resp.json();
+            console.log(user);
+        } else {
+            alert("Invalid email!")
+        }
+    }
+
     const onLogout = () => {
         localStorage.clear();
         setLoggedInUser(null);
     };
+
     return (
         <Routes>
             <Route path='/*'
-                   element={loggedInUser ? <MainPage loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} onLogout={onLogout}/> :
-                            loggedInUser === undefined ? <div>Loading...</div> :
+                   element={loggedInUser ?
+                       <MainPage loggedInUser={loggedInUser} setLoggedInUser={setLoggedInUser} onLogout={onLogout}/> :
+                       loggedInUser === undefined ? <div>Loading...</div> :
                            <Navigate replace to={"/login"}/>}/>
             <Route exact path='/login'
-                   element={<LoginPage onLogin={onLogin}/>}/>
+                   element={<LoginPage loggedInUser={loggedInUser} onLogin={onLogin}/>}/>
+            <Route exact path='/registration'
+                   element={<RegistrationPage loggedInUser={loggedInUser} onRegistration={onRegistration}/>}/>
         </Routes>
     );
 }
