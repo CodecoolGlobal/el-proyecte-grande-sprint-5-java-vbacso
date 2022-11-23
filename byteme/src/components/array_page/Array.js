972e179a821b28bd6c8025fb9f77b0e749390821
fergencszeno <fergencszeno@gmail.com>
2022-11-23 13:53:30 +0100

    feat: create Array component

---
+++ byteme/src/components/array_page/Array.js
@@ -0,0 +1,32 @@
+const Array = ({group, showedUser, placement}) => {
+
+    const isOwnerPlacement = () => {
+        if (group.owner.id === showedUser.id && placement === "owner") {
+            return true;
+        } else if (group.owner.id === showedUser.id && placement === "member") {
+            return false;
+        } else {
+            return false;
+        }
+    };
+
+    const isMemberPlacement = () => {
+        for (let member of group.members) {
+            if (member.id === showedUser.id && group.owner.id === showedUser.id) {
+                return false;
+            } else if (member.id === showedUser.id && placement === "member") {
+                return true;
+            }
+        }
+        return false;
+    };
+
+    return (
+        <li className="nav-item array">
+            {isOwnerPlacement() ? <p className="owner-array">{group.name}</p> : isMemberPlacement() ?
+                <p className="member-array">{group.name}</p> : <div></div>}
+        </li>
+    );
+};
+
+export default Array;
