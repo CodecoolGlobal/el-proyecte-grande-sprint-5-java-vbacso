8f2afa0e6c65cc00e6110a1084d179ec21caa80a
vbacso <bacsovanda95@gmail.com>
2022-11-24 15:02:10 +0100

    feat: implement JWT config with config properties

--- src/main/java/com/codecool/byteMe/ByteMeApplication.java
+++ src/main/java/com/codecool/byteMe/ByteMeApplication.java
@@ -1,9 +1,12 @@
 package com.codecool.byteMe;
 
+import com.codecool.byteMe.security.JwtConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(JwtConfiguration.class)
 public class ByteMeApplication {
     public static void main(String[] args) {
         SpringApplication.run(ByteMeApplication.class, args);
