5dc65d485e78147e6c76065c779d04e9174ece49
vbacso <bacsovanda95@gmail.com>
2023-01-12 10:57:13 +0100

    refact: delete empty hook from DisplayUserDatas

--- byteme/src/components/user_page/DisplayUserDatas.js
+++ byteme/src/components/user_page/DisplayUserDatas.js
@@ -1,10 +1,4 @@
-import React, {useEffect} from 'react';
-
-const DisplayUserDatas = ({showedUser, setShowedUser}) => {
-    useEffect(() => {
-
-    }, [setShowedUser]);
-
+const DisplayUserDatas = ({showedUser}) => {
     return (
         <div className="user-details-container">
             <div className="details">Name: {showedUser.name}</div>
